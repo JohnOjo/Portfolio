ae14bc28a10dc9c687ef0f47d2abb2517059aef1
johnojo97 <jojo@retrorabbit.co.za>
2022-04-26 18:45:35 +0200

    Added mobile nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,33 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import './Header.scss'
 import { BIO, FAVOURTIE_PROJECTS, OTHER_PROJECTS, TECHNOLOGIES_USED, SCROLL_ELEMENT } from '../../constants/headerConstants'
+import MobileNavDropdown from '../../assets/mobile-nav-dropdown.svg'
+import MobileCloseDropdown from '../../assets/mobile-close-dropdown.svg'
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isDesktop: false,
+            dropdownOpen: false,
+        }
+
+        this.updatePredicate = this.updatePredicate.bind(this)
+    }
+
+    componentDidMount() {
+        this.updatePredicate()
+        window.addEventListener('resize', this.updatePredicate)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updatePredicate)
+    }
+
+    updatePredicate() {
+        this.setState({ isDesktop: window.innerWidth > 704 })
+        window.innerWidth > 704 && this.setState({ dropdownOpen: false })
+    }
     renderHeaderButton(headerButton) {
         return (
             <div
@@ -64,7 +89,23 @@ class Header extends Component {
         }
     }
 
+    renderMobileHeaderButton(headerButton) {
+        return (
+            <div
+                key={headerButton}
+                className={'header-text mobile-header-button'}
+                onClick={() => this.handleClick(headerButton)}
+            >
+                <div className={'header-item-text'}>{headerButton}</div>
+                <div className={'header-underline'}>
+                </div>
+            </div>
+        )
+    }
+
     render() {
+        const isDesktop = this.state.isDesktop
+
         return (
             <div className={'header'}>
                 <div className={'header-text-container'}>
@@ -73,9 +114,17 @@ class Header extends Component {
                     </div>
                 </div>
                 <div className={'f-1'}/>
-                <div className={'buttons-container'}>
+                {/* <div className={'buttons-container'}>
                     {this.renderHeaderButtons()}
-                </div>
+                </div> */}
+                {(!isDesktop && !this.state.dropdownOpen) && <img className={'dropdown'} src={MobileNavDropdown} alt={'dropdown'} onClick={() => this.handleMobileDropdown()}/>}
+                {isDesktop && this.renderHeaderButtons()}
+                {(!isDesktop && this.state.dropdownOpen) &&
+				<div className={'mobile-header-buttons'}>
+				    {(!isDesktop && this.state.dropdownOpen) && <img className={'dropdown-close'} src={MobileCloseDropdown} alt={'dropdown'} onClick={() => this.handleMobileDropdown()}/>}
+				    {this.renderMobileHeaderButtons()}
+				</div>
+                }
             </div>
         )
     }
